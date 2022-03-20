a5b616c6de2f0b7e2caf4334519530c61166d7ee
Vinicius Port <caprilesport@gmail.com>
2022-03-20 17:52:01 -0300

    neovim plugins modified and changes to env

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -39,10 +39,10 @@ return require('packer').startup(function()
   use 'vimwiki/vimwiki'
 
   -- Minimap 
-  use 'wfxr/minimap.vim' 
-  vim.g.minimap_width = '10'
-  vim.g.minimap_auto_start = '1'
-  vim.g.minimap_auto_start_win_enter = '1'
+  -- use 'wfxr/minimap.vim' 
+  -- vim.g.minimap_width = '10'
+  -- vim.g.minimap_auto_start = '1'
+  -- vim.g.minimap_auto_start_win_enter = '1'
 
   -- Julia
   use 'JuliaEditorSupport/julia-vim'
--- .zshrc
+++ .zshrc
@@ -25,7 +25,7 @@ source ~/.env
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-# [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 
 
